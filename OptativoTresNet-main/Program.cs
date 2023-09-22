3767038a27cabe794f8f8fd0bcbf2b69ee9fb4bb
Felipe Daniel Baranda Armoa <78660949+FelipeDanielBarandaArmoa@users.noreply.github.com>
2023-09-21 21:16:57 -0400

    agregando carpetas y archivos

---
+++ OptativoTresNet-main/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Infraestructura.Conexiones;
+using Servicios.ContactosService;
+
+CiudadService ciudadService = new CiudadService("Server=localhost;Port=5432;User Id=postgres;Password=Century123456;Database=parcial;");
+
+
+var ciudad = ciudadService.obtenerCiudad(2);
+Console.WriteLine($"Descripcion: {ciudad.descripcion}, nombre corto: {ciudad.nombre_corto}");
+
+ciudad.descripcion = "Mariano Roque Alonso";
+ciudadService.modificarCiudad(ciudad);
+
+Console.WriteLine("...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ OptativoTresNet-main/Servicios/ContactosService/ClienteService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Infraestructura.Datos;
+using Infraestructura.Modelos;
+
+namespace Servicios.ClienteService
+{
+    public class ClienteService
+    {
+        ClienteDatos clienteDatos;
+
+        public ClienteService(string cadenaConexion)
+        {
+            clienteDatos = new ClienteDatos(cadenaConexion);
+        }
+
+        public void InsertarCliente(ClienteModel cliente)
+        {
+            ValidarDatos(cliente);
+            clienteDatos.InsertarCliente(cliente);
+        }
+
+        private void ValidarDatos(ClienteModel cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteModel ObtenerCliente(int id)
+        {
+            return clienteDatos.ObtenerClientePorId(id);
+        }
+
+        /*public void ModificarCliente(ClienteModel cliente)
+        {
+            ValidarDatos(cliente);
+            clienteDatos.ModificarCliente(cliente);
+        }*/
+
+       /* private void ValidarDatos(ClienteModel cliente)
+        {
+            // Aquí puedes agregar validaciones adicionales según tus requerimientos.
+            if (string.IsNullOrWhiteSpace(cliente.Nombre) || string.IsNullOrWhiteSpace(cliente.Apellido))
+            {
+                throw new Exception("El nombre y el apellido no pueden estar en blanco.");
+            }
+        }*/
+    }
+}
